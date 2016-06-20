e32b6ec3e65fef0cf34fd9a610bf2df753f7a892
box327 <kibox327@gmail.com>
2016-06-20 22:57:21 +0900

    a

---
+++ android/app/src/main/java/com/example/shjan/embedded/IdealTemperatureGenerator.java
@@ -0,0 +1,16 @@
+package com.example.shjan.embedded;
+
+/**
+ * Created by Administrator on 2016-06-20.
+ */
+public class IdealTemperatureGenerator {
+    public int getIdealTemperature()
+    {
+        return 25;
+    }
+
+    public void setManualTemperature(int temperature)
+    {
+
+    }
+}
